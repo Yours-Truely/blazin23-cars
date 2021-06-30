099afef3d9608a1e966813e9c42a29ff554a9298
Yours-Truely <77418144+Yours-Truely@users.noreply.github.com>
2021-06-30 12:15:51 +0200

    Adding 3

---
+++ client/vehicle_name.lua
@@ -0,0 +1,6 @@
+Citizen.CreateThread(function()
+    AddTextEntry("zodiac", "Vulcar Zodiac"),
+    AddTextEntry("zodiacr", "Vulcar Zodiac Racer"),
+    AddTextEntry("zodiacc", "Vulcar Zodiac Camper"),
+    AddTextEntry("argento", "Obey Argento"),
+end)
